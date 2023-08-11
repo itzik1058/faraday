12eff1cfc4c666daedbdf69a40763343e7fc41cc
Itzik <itzik1058@gmail.com>
2023-08-11 09:40:18 +0300

    jinja2 templates

---
+++ render.py
@@ -0,0 +1,19 @@
+from jinja2 import Environment, FileSystemLoader
+from dotenv import dotenv_values
+from pathlib import Path
+
+if __name__ == "__main__":
+    env = Environment(
+        loader=FileSystemLoader(Path()),
+        variable_start_string="{@",
+        variable_end_string="@}",
+    )
+    variables = dotenv_values()
+    for path in Path().rglob("*.j2"):
+        template = env.get_template(str(path))
+        rendered = template.render(**variables)
+        path.with_suffix("").write_text(rendered)
+    for path in Path().rglob("*.env"):
+        path.with_suffix(".example").write_text(
+            "\n".join([f"{key}=" for key in variables])
+        )
